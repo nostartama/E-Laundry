2d37ffa040a9cb0dbbdec71dd70ba005adffec74
Nikolaus Aldo Halim <Nikolausaldoh@gmail.com>
2017-08-25 11:59:57 +0700

    java

--- app/src/main/java/com/example/tiora/e_laundry/MainActivity.java
+++ app/src/main/java/com/example/tiora/e_laundry/MainActivity.java
@@ -2,12 +2,31 @@ package com.example.tiora.e_laundry;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    int blankets;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void increase(View view) {
+        blankets++;
+        display(blankets);
+    }
+
+    public void decrease(View view) {
+        blankets--;
+        display(blankets);
+
+    }
+
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
     }
 }
